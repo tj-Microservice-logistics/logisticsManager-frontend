8dff40f0a02a10672416bc810f9eb44d07f7d5fa
unknown <28660592@qq.com>
2024-12-26 03:04:03 +0800

    LJC

--- src/api/warehouses.ts
+++ src/api/warehouses.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const getWarehouses = (page: number = 0, size: number = 50) => {
-    return axios.get('http://localhost:9081/warehouses', {
+    return axios.get('http://100.80.117.24:9082/warehouses', {
         timeout: 10000, // 设置合理的超时时间
         headers: {
             'Content-Type': 'application/json'
